ce25696af1ba0b2b1c7a26a1939cb7ad945629b0
ryanream <ryan.a.ream@gmail.com>
2020-10-15 11:29:46 -0700

    add routes, build auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const config = require('config');
+const bcrypt = require('bcryptJS');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
 
 // @route   GET api/auth
 // @desc    Get logged in user
@@ -11,8 +17,57 @@ router.get('/', (req, res) => {
 // @route   POST api/auth
 // @desc    Auth user & get token
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Log in user');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email.').isEmail(),
+    check('password', 'Password is required.').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    // authentication
+    try {
+      // is email valid?
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Credentials ' });
+      }
+
+      // is password (hash) valid? ** why are we using isMatch? Is that a normal variable for this scenario? **
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+
+      // if email/pass clear we'll get the user id
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      // send id (payload) and jwtSecret to jwt
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        // return the token (password hash) if no error
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.satus(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware - here we can accept data
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the contact keeper API...' })
@@ -8,8 +15,8 @@ app.get('/', (req, res) =>
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
